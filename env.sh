dd403e4d058ed8a29fc457bb829a2331f2709230
panyao <pan3yao@gmail.com>
2022-06-28 10:21:09 +0800

    change env.sh

--- env.sh
+++ env.sh
@@ -7,10 +7,13 @@ echo work dir [$homePath] [$workPath]
 export GO111MODULE=on
 export GOPATH=$homePath/
 export GOROOT=$GOPATH/go
+export GOPROXY=https://goproxy.cn
 
 [ -d $GOROOT ] || {
     echo "no go env"
-    wget https://golang.org/dl/go1.16.linux-amd64.tar.gz
+    pushd $GOPATH
+    wget https://go.dev/dl/go1.16.linux-amd64.tar.gz
     tar -zxvf go1.16.linux-amd64.tar.gz
+    popd
 }
 export PATH=$GOROOT/bin:$PATH
